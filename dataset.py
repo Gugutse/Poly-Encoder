2c32e7b3cd84a2bc34b02b0e7a21f65757090b09
Daria Bakshandaeva <43073018+Gugutse@users.noreply.github.com>
2020-09-30 21:02:41 +0300

    Update dataset.py

--- dataset.py
+++ dataset.py
@@ -7,28 +7,35 @@ import pickle
 
 
 class SelectionDataset(Dataset):
-    def __init__(self, file_path, context_transform, response_transform, concat_transform, sample_cnt=None, mode='poly'):
+    def __init__(self, file_path, context_transform, response_transform, concat_transform, sample_cnt=None, mode='poly', dist=False):
         self.context_transform = context_transform
         self.response_transform = response_transform
         self.concat_transform = concat_transform
         self.data_source = []
         self.mode = mode
+        self.dist = dist
+        
         neg_responses = []
         with open(file_path, encoding='utf-8') as f:
             group = {
                 'context': None,
                 'responses': [],
-                'labels': []
+                'labels': [],
+                'logits': []
             }
             for line in f:
                 split = line.strip().split('\t')
-                lbl, context, response = int(split[0]), split[1:-1], split[-1]
+                if self.dist:
+                    logits, lbl, context, response = [float(split[0]), float(split[1])], int(split[2]), split[3:-1], split[-1]
+                else:
+                    lbl, context, response = int(split[0]), split[1:-1], split[-1]
                 if lbl == 1 and len(group['responses']) > 0:
                     self.data_source.append(group)
                     group = {
                         'context': None,
                         'responses': [],
-                        'labels': []
+                        'labels': [],
+                        'logits': []
                     }
                     if sample_cnt is not None and len(self.data_source) >= sample_cnt:
                         break
@@ -37,6 +44,8 @@ class SelectionDataset(Dataset):
                 group['responses'].append(response)
                 group['labels'].append(lbl)
                 group['context'] = context
+                if self.dist:
+                    group['logits'].append(logits)
             if len(group['responses']) > 0:
                 self.data_source.append(group)
 
@@ -45,14 +54,17 @@ class SelectionDataset(Dataset):
 
     def __getitem__(self, index):
         group = self.data_source[index]
-        context, responses, labels = group['context'], group['responses'], group['labels']
+        context, responses, labels, logits = group['context'], group['responses'], group['labels'], group['logits']
         if self.mode == 'cross':
             transformed_text = self.concat_transform(context, responses)
             ret = transformed_text, labels
         else:
             transformed_context = self.context_transform(context)  # [token_ids],[seg_ids],[masks]
             transformed_responses = self.response_transform(responses)  # [token_ids],[seg_ids],[masks]
-            ret = transformed_context, transformed_responses, labels
+            if self.dist:
+                ret = transformed_context, transformed_responses, labels, logits
+            else:
+                ret = transformed_context, transformed_responses, labels
 
         return ret
 
