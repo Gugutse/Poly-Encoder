229825a24716d9f3ce9a62eed8db79968740d1ee
Daria Bakshandaeva <43073018+Gugutse@users.noreply.github.com>
2020-09-30 21:22:23 +0300

    Update encoder.py

--- encoder.py
+++ encoder.py
@@ -4,6 +4,19 @@ import torch.nn as nn
 import torch.nn.functional as F
 from transformers import BertPreTrainedModel, BertModel
 
+class DistillationLoss(torch.nn.Module):
+    def __init__(self, temperature: float = 1.):
+        super().__init__()
+        self.temperature = 1.
+        
+    def forward(self, student_logits, teacher_logits):
+        
+        teacher_prediction = torch.exp(torch.log_softmax(teacher_logits / self.temperature, dim=-1))
+        student_prediction = torch.log_softmax(student_logits / self.temperature, dim=-1)
+        loss = torch.mean(torch.sum(-teacher_prediction * student_prediction, dim=-1))
+        
+        return loss
+
 
 class BiEncoder(BertPreTrainedModel):
     def __init__(self, config, *inputs, **kwargs):
