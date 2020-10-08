2747e43d791ad5c79ff636c24147587a2dbe109c
Daria Bakshandaeva <43073018+Gugutse@users.noreply.github.com>
2020-10-08 07:51:32 +0300

    Update encoder.py

--- encoder.py
+++ encoder.py
@@ -55,7 +55,7 @@ class CrossEncoder():
         if labels is not None:
             labels = labels.view(-1)
             loss = self.criterion(prediction, labels)
-            return loss
+            return prediction, loss
         else:
             return prediction
 
